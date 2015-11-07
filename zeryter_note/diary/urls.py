a7df2c1412d55d97acce4465742e6de0dfa2e079
zeryter <zeryter@hotmail.com>
2015-11-08 03:14:11 +0800

    添加了note项目

--- zeryter_note/diary/urls.py
+++ zeryter_note/diary/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'zeryter_note.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'', 'diary.webpage.homepage'),
+    url(r'^$', 'diary.webpage.homepage'),
+    url(r'^diary/(?P<pk>[0-9]+)/$','diary.webpage.diary_views'),
 )
 
--- zeryter_note/diary/webpage.py
+++ zeryter_note/diary/webpage.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-from diary.models import Diary
-from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
+from .models import Diary
 
 # Create your views here.
 
 def homepage(request):
     diarys = Diary.objects.filter(refresh_time__isnull=False)
-    user = User.objects.get(id=1)
-    return render(request, 'home.html', {'diarys':diarys, 'user':user})
+    return render(request, 'home.html', {'diarys':diarys})
+
+def diary_views(request, pk):
+    di = get_object_or_404(Diary, pk=pk)
+    return render(request, 'diary_page.html', {'di': di})
 
 
