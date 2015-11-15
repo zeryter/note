48d75bc217c68fd06065020850ec801a136a5deb
zeryter <zeryter@hotmail.com>
2015-11-15 12:43:56 +0800

    修改了部分bug

--- zeryter_note/diary/urls.py
+++ zeryter_note/diary/urls.py
@@ -5,11 +5,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'zeryter_note.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'diary.webpage.homepage'),
+    url(r'^$', 'diary.webpage.homepage', name='homepage'),
     url(r'^diary/(?P<pk>[0-9]+)/$','diary.webpage.diary_views'),
     url(r'^diary/new/$', 'diary.webpage.diary_new', name='diary_new'),
     url(r"^post/(?P<pk>[0-9]+)/edit/$", 'diary.webpage.diary_edit', name='diary_edit'),
     url(r'^draft/', 'diary.webpage.diary_draft', name='diary_draft'),
+    url(r'^publish/(?P<pk>[0-9]+)/$', 'diary.webpage.diary_publish', name='diary_publish'),
 
 
 
--- zeryter_note/diary/webpage.py
+++ zeryter_note/diary/webpage.py
@@ -1,46 +1,66 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 from .models import Diary
 from .forms import DiaryForm
 
-# Create your views here.
 
+# 主页，所有文章列表
 def homepage(request):
     diarys = Diary.objects.filter(refresh_time__isnull=False)
-    return render(request, 'home.html', {'diarys':diarys})
+    return render(request, 'home.html', {'diarys': diarys})
 
+
+# 文章详情
 def diary_views(request, pk):
     di = get_object_or_404(Diary, pk=pk)
     return render(request, 'diary_page.html', {'di': di})
 
+
+# 新建文章
 def diary_new(request):
     if request.method == "POST":
         form = DiaryForm(request.POST)
+        checkbox_list = request.REQUEST.getlist('diary')
         if form.is_valid():
             diary = form.save(commit=False)
-            diary.auther = request.user
+            diary.auther = User.objects.get(id=2)
+            print(form)
+            print(checkbox_list)
+            diary.auther = request.user  # TODO这里需要登录功能处理
+            if checkbox_list and checkbox_list[0] == '草稿箱':
+                diary.save()
+                return redirect('diary.webpage.diary_draft')
             diary.refresh()
             return redirect('diary.webpage.homepage')
-
     else:
-            form = DiaryForm()
-    return render(request, 'diary_edit.html', {'form':form})
+        form = DiaryForm
+    return render(request, 'diary_edit.html', {'form': form})
 
+
+# 内容编辑
 def diary_edit(request, pk):
-    print("test1") #TODO.......
     diary = get_object_or_404(Diary, pk=pk)
-    print("test2") #TODO.......
     if request.method == "POST":
-        form = DiaryForm(request.post, diary)
-        diary.auther = request.user
-        diary.refresh()
-        return redirect('diary.webpage.homepage', pk = diary.pk)
+        form = DiaryForm(request.POST, instance=diary)
+        if form.is_valid():
+            diary = form.save(commit=False)
+            # diary.auther = request.user
+            diary.auther = User.objects.get(id=2)
+            diary.save()
+        return redirect('diary.webpage.homepage', pk=diary.pk)
     else:
         form = DiaryForm(instance=diary)
-        return render(request, 'diary_edit.html', {'form':form})
+    return render(request, 'diary_edit.html', {'form': form})
 
+
+# 草稿箱列表
 def diary_draft(request):
     diary_drafts = Diary.objects.filter(refresh_time__isnull=True).order_by('-creat_time')
-    return render(request, 'draftpage.html', {'drafts':diary_drafts})
+    return render(request, 'draftpage.html', {'drafts': diary_drafts})
 
 
+# 草稿箱发布功能
+def diary_publish(pk):
+    diary = get_object_or_404(Diary, pk=pk)
+    diary.refresh()
+    return redirect('diary.webpage.homepage')
